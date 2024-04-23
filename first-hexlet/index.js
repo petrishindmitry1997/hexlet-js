b62051557c49c7a227c38bf2a77345cf5f2397a2
dp <petrishindmitry1997@gmail.com>
2024-04-23 13:15:44 +0300

    point of entry

--- first-hexlet/index.js
+++ first-hexlet/index.js
@@ -1,3 +1,7 @@
 import _ from 'lodash';
+import getBirthYear from './src/index.js';
 
-console.log(_.last(['one', 'two']));
+console.log('Сколько вам лет?');
+const age = 18; // ...
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
---
+++ first-hexlet/src/index.js
@@ -0,0 +1,5 @@
+export default function getBirthYear(age) {
+    const birthYear = 2005; // year of birth
+  
+    return birthYear;
+  };
