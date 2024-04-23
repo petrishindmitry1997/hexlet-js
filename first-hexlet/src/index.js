53365ef5480dc26806011eade69af24e61ab191c
dp <petrishindmitry1997@gmail.com>
2024-04-23 13:49:24 +0300

    adding ESlint & Prettier

--- first-hexlet/src/index.js
+++ first-hexlet/src/index.js
@@ -1,5 +1,5 @@
 export default function getBirthYear(age) {
-    const birthYear = 2005; // year of birth
-  
-    return birthYear;
-  };
+  const birthYear = 2005; // year of birth
+
+  return birthYear;
+};
