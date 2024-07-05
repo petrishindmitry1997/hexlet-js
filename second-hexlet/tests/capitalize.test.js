4589b67a21bb7a8bc92c4d807fc3722e3933f95e
dp <petrishindmitry1997@gmail.com>
2024-07-05 11:42:49 +0300

    add assert

--- second-hexlet/tests/capitalize.test.js
+++ second-hexlet/tests/capitalize.test.js
@@ -1,14 +1,10 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
-
+import { strict as assert } from 'node:assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
